b6006cd8451fa9776efb88ab91f563372a5ae4ff
Anthony FERREYROLLES <anthony.ferreyrolles@edu.ece.fr>
2022-01-20 12:37:29 +0100

    Update bluetooth.h

--- models/bluetooth.h
+++ models/bluetooth.h
@@ -11,6 +11,10 @@
 
 #include <stdio.h>
 
-void initializeBluetooth(void);
+bool initializeBluetooth(void);
+sdp_session_t *register_service(uint8_t rfcomm_channel);
+int init_server();
+char *read_server(int client);
+void write_server(int client, char *message);
 
 #endif /* bluetooth_h */
