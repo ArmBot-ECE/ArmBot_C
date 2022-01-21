ece6d3ad1c2098a6f49490a7f0e1a87c7b627e47
LeNiokeur <58481068+LeNiokeur@users.noreply.github.com>
2022-01-21 11:42:42 +0100

    [UPDATE] Steppermotor API

--- models/stepperMotorControl.c
+++ models/stepperMotorControl.c
@@ -36,9 +36,19 @@ void incrementAngleStepper(int *currentAngle, int steppermotorPosition) {
 }
 
 void incrementAngleTest() {
+    printf("Test XMOVE 0 XDIR 1\n");
     digitalWrite(XMOVE,0);
     digitalWrite(XDIR,1);
-    printf("Increment Angle\n");
+    printf("Test XMOVE 1 XDIR 1\n");
+    digitalWrite(XMOVE,1);
+    digitalWrite(XDIR,1);
+    printf("Test XMOVE 1 XDIR 0\n");
+    digitalWrite(XMOVE,1);
+    digitalWrite(XDIR,0);
+    printf("Test XMOVE 0 XDIR 0\n");
+    digitalWrite(XMOVE,0);
+    digitalWrite(XDIR,0);
+
 }
 
 void decrementAngleStepper(int *currentAngle, int steppermotorPosition) {
