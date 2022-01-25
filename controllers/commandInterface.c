88d78fa0b612ab44e778d30f194115632b2bf29b
LeNiokeur <58481068+LeNiokeur@users.noreply.github.com>
2022-01-25 10:42:20 +0100

    [UPDATE] Integration bluetooth.c

--- controllers/commandInterface.c
+++ controllers/commandInterface.c
@@ -19,14 +19,14 @@
 */
 void moveForwardButton() {
     setDirectionStepper(1, 1);
-    setDirectionStepper(2, 0);
+//    setDirectionStepper(2, 0);
     for(int i=0; i<50; i++) {
         rotateStepper(1);
         delay(5);
-        rotateStepper(2);
-        delay(5);
-        rotateStepper(2);
-        delay(5);
+//        rotateStepper(2);
+//        delay(5);
+//        rotateStepper(2);
+//        delay(5);
     }
 
 }
